b018ee266ea8b955773ea3f6102221221570c9c9
HideonJava <m18890248550@163.com>
2022-07-29 11:46:55 +0800

    add .gitignore

---
+++ src/views/permission/components/gxbg.vue
@@ -0,0 +1,106 @@
+<template>
+  <div>
+    <div ref="arank" :style="{ height: height, width: width }" />
+  </div>
+</template>
+
+<script>
+import * as echarts from 'echarts'
+import ResizeMixin from '../../../../components/Charts/mixins/resize'
+
+export default {
+  mixins: [ResizeMixin],
+  props: {
+    width: {
+      type: String,
+      default: '100%'
+    },
+    height: {
+      type: String,
+      default: '400px'
+    }
+  },
+  data() {
+    return {
+      chart: null
+    }
+  },
+  mounted() {
+    // this.chart = echarts.init(this.$refs['arank'], 'walden')
+    // this.chart.showLoading({
+    //   text: '加载中...',
+    //   color: '#09F7F7',
+    //   textColor: '#909399',
+    //   fontSize: '14px'
+    // })
+  },
+  beforeDestroy() {
+    if (!this.chart) {
+      return
+    }
+    this.chart.dispose()
+    this.chart = null
+  },
+  methods: {
+    initChart(data) {
+      if (data.jjRank.datas.length > 0) {
+        this.chart = echarts.init(this.$refs['arank'], 'walden')
+        this.chart.showLoading({
+          text: '加载中...',
+          color: '#09F7F7',
+          textColor: '#909399',
+          fontSize: '14px'
+        })
+        this.chart.hideLoading()
+        this.chart.setOption({
+          tooltip: {
+            trigger: 'axis',
+            axisPointer: {
+              // Use axis to trigger tooltip
+              type: 'shadow' // 'shadow' as default; can also be 'line' or 'shadow'
+            }
+          },
+          legend: {},
+          grid: {
+            left: '3%',
+            right: '4%',
+            bottom: '3%',
+            containLabel: true
+          },
+          xAxis: {
+            type: 'value'
+          },
+          yAxis: {
+            type: 'category',
+            data: data.jjRank.users
+          },
+          series: [
+            {
+              name: '进阶能力分',
+              type: 'bar',
+              stack: 'total',
+              label: {
+                show: true
+              },
+              emphasis: {
+                focus: 'series'
+              },
+              data: data.jjRank.datas
+            }
+          ]
+        })
+      } else {
+        this.chart.setOption({}, true)
+        this.chart.showLoading({
+          text: '暂无数据',
+          color: '#fff',
+          textColor: '#909399',
+          fontSize: '14px'
+        })
+      }
+    }
+  }
+}
+</script>
+<style>
+</style>
